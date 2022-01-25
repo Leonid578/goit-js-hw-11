ffbd532cadf6c1079dac611085fae7272f6b71c1
Leonid578 <Leonid.Ivanov187@gmail.com>
2022-01-25 18:24:45 +0200

    6

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './css/styles.css';
 import axios from 'axios';
 import { SendImg } from './js/classSearch';
-import { addImg } from './js/htmlSimpleLightbox';
-
+import { addImg } from './js/htmlRender';
+import { scroll } from './js/scroll';
 import { remove } from './js/htmlRemove';
 import Notiflix from 'notiflix';
 
@@ -19,7 +19,7 @@ async function onFetchForm(e) {
   let valueInput = document.querySelector('#inputimg').value;
   if (valueInput.trim() === '') {
     remove(render);
-    console.log('Error')
+    // console.log('Error')
   } else {
     remove(render);
     sendImg.startPages();
@@ -45,7 +45,7 @@ async function onFetchForm(e) {
         try {
           arrImg = await sendImg.fetchImg(sendImg.metodSend, sendImg.numberPages);
           addImg(arrImg);
-          // scroll();
+          scroll();
 
           if (arrImg.totalHits / sendImg.pages > sendImg.numberPages) {
             Notiflix.Notify.success(`Hooray! We found more ${sendImg.pages} images.`);
